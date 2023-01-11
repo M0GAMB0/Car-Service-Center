87b373bdf0eeea2332891da45d1e90a8df42d353
Saikumar Hangal⚡ <89583145+M0GAMB0@users.noreply.github.com>
2023-01-11 23:36:46 +0530

    Update JalanTech.java

--- JalanTech.java
+++ JalanTech.java
@@ -129,7 +129,7 @@ class CarService{
     }
 
 }
-public class JalanTech {
+public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         while(true){
